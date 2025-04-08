5708480b389a7dd91d7f0ec37d27a8c324d3fffd
ishangiri <ishansmith2@gmail.com>
2025-04-08 20:55:21 +1000

    changed api endpoint for production

--- client/src/utils/fetchUtil.js
+++ client/src/utils/fetchUtil.js
@@ -1,7 +1,13 @@
 import axios from "axios";
+import  dotenv from "dotenv";
+
+dotenv.config();
+
+const VITE_API_URL = process.env.VITE_API_URL ;
 
 const fetchData = axios.create({
-    baseURL : '/api'
+    baseURL : VITE_API_URL+'/api',
+    withCredentials: true,
 })
 
 export default fetchData;
