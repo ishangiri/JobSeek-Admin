3cd316a6fe0606a9f2eb7e3ee4ed2585bdf0dbec
ishangiri <ishansmith2@gmail.com>
2025-04-08 21:07:44 +1000

    APi issue fixed

--- client/src/utils/fetchUtil.js
+++ client/src/utils/fetchUtil.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fetchData = axios.create({
-  baseURL: import.meta.env.VITE_API_URL+'/api', // Works in dev & prod
+  baseURL: import.meta.env.VITE_API_URL, // Works in dev & prod
   withCredentials: true, 
 });
 
